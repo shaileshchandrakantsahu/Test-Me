8ca40c24a5f0aba2120270ba220e5da654370c3a
shaileshchandrakantsahu <shailesh.sahu@cumminscollege.edu.in>
2022-03-02 15:53:32 +0530

    fist commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Hello from './Hello.js';
+function App() {
+  return (
+    <div className="App">
+
+      <h1> Test Me </h1>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
